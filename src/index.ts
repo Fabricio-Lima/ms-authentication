b4cbb6b03f3ad3b2724ffd7b09148cce89165770
Fabricio Lima <fabriciolc61@gmail.com>
2021-12-22 00:28:20 -0300

    project init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction ) => {
+    res.status(200).send({ foo: 'bar' });
+});
+
+app.listen(3000, () => {
+    console.log('[+] Server is running on port 3000');
+});
