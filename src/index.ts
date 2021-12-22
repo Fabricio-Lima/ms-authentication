c2cc81f0732afc07488f5972beb76e8ac9ab43e9
Fabricio Lima <fabriciolc61@gmail.com>
2021-12-22 00:59:42 -0300

    initialize eslint

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ app.get('/status', (req: Request, res: Response, next: NextFunction ) => {
 });
 
 app.listen(3000, () => {
-    console.log('[+] Server is running on port 3000');
+    console.log('\n[+] Server is running on port 3000...\n');
 });
